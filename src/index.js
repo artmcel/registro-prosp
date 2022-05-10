cdbae78c6e9803d598a27bddf6088e87154c151b
WebMaster <umrec_web@unimex.edu.mx>
2022-05-10 18:48:33 -0500

    load component module

--- src/index.js
+++ src/index.js
@@ -6,18 +6,20 @@
 import './styles.css';
 //import bootsrap si lo necesitas
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './js/componentes.js';
  
  //cargadinamica ejemplo:
  //const mostrarMensaje = () => import( /* webpackChunkName: "peticiones" */ './services/peticiones' );
 
 // carga dinamica de modulo logo, silo necesitas..
 /*
-import( './js/logo' ).then( module =>{
+import( './js/componentes' ).then( module =>{
 
-    const cambiaLogo = module.cambioLogo;
-    cambiaLogo();
+    const load = module.getCookie();
+    load();
 });
 */
+
 /**
  * importamos el modulo de forma simple para que no ejecute como funcion, sino hasta que sea llamado en el componentes.js
  * 
