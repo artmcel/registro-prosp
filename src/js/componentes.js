9ab81e6ec4dd675893e45dc257206603973f0d42
WebMaster <umrec_web@unimex.edu.mx>
2022-05-10 18:48:56 -0500

    logic for formulario

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,3 +1,64 @@
 /**
  * logica del proyecto
  */
+
+const boton = document.getElementById('boton'),
+      inputs = document.querySelectorAll('input'),
+      selects = document.querySelectorAll('select');
+
+let datos = {};
+
+/**
+ * 
+ * obtenemos las cookies de las url y las guardamos en una variable
+ * 
+ */
+
+window.addEventListener('load', ()=>{
+    const url = new URLSearchParams(window.location.search);
+          cookies = {};
+    
+    url.forEach( (value, key) => {
+        
+
+        document.cookie = `${key}=${value};expires=Thu, 31 Dec 2022 12:00:00 UTC;`;
+
+        datos = { ...datos, [key]: value };	
+
+    });
+    //console.log(cookies);
+});
+
+/**
+ * 
+ * logica del envio de informacion
+ * 
+ */
+boton.addEventListener('click', (e)=>{
+
+    e.preventDefault();
+
+    //let datos = {};
+    
+    inputs.forEach( ({ id, value }) => {
+
+        if(!value) throw 'Todos los campos son obligatorios';
+
+        datos = { ...datos, [id]: value };
+        
+    });
+
+    selects.forEach( ({ id, value }) => {
+
+        //if(!value) throw 'Todos los campos son obligatorios';
+
+        datos = { ...datos, [id]: value } ;
+
+    });
+
+    console.log(datos);
+
+
+
+});
+
