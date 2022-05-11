cd55641a9c564d58017f078f0f05ca7c996ac372
WebMaster <umrec_web@unimex.edu.mx>
2022-05-11 13:49:41 -0500

    fetch obtener planteles and niveles

--- src/js/services/peticiones.js
+++ src/js/services/peticiones.js
@@ -1,3 +1,41 @@
 /**
  * archivo para peticiones fetch...
  */
+
+/**
+ * obtener planteles desde php
+ */
+
+const url = `http://192.168.1.123/sidetrack/php/peticiones.php`;
+
+const obtenerPlanteles = async () => {
+
+    const respuesta = await fetch(`${url}?planteles`);	
+
+    if(!respuesta) throw 'No se pudo obtener los planteles';
+
+    const planteles = await respuesta.json();
+    return planteles;
+
+}
+
+const obtenerNiveles = async ( plantel ) => {
+
+    const respuesta = await fetch(`${url}?niveles`, {
+        method: 'POST',
+        body: JSON.stringify({plantel})
+    });
+
+    if(!respuesta) throw 'No se pudo obtener los niveles';
+
+    const niveles = await respuesta.json();
+    //console.log(niveles);
+    return niveles;
+}
+
+
+
+export {
+    obtenerPlanteles,
+    obtenerNiveles
+}
