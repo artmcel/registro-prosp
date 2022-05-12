d0860f2675490910ed2b521781292c7c12822277
WebMaster <umrec_web@unimex.edu.mx>
2022-05-12 13:21:00 -0500

    add the respuesta.ok

--- src/js/services/peticiones.js
+++ src/js/services/peticiones.js
@@ -12,7 +12,7 @@ const obtenerPlanteles = async () => {
 
     const respuesta = await fetch(`${url}?planteles`);	
 
-    if(!respuesta) throw 'No se pudo obtener los planteles';
+    if(!respuesta.ok) throw 'No se pudo obtener los planteles';
 
     const planteles = await respuesta.json();
     return planteles;
@@ -23,7 +23,7 @@ const obtenerNiveles = async ( plantel ) => {
 
     const respuesta = await fetch(`${url}?obtener=niveles&idPlantel=${plantel}`);
 
-    if(!respuesta) throw 'No se pudo obtener los niveles';
+    if(!respuesta.ok) throw 'No se pudo obtener los niveles';
 
     const niveles = await respuesta.json();
     //console.log(niveles);
@@ -34,7 +34,7 @@ const obtenerPeriodos = async ( plantel ) => {
 
     const respuesta = await fetch(`${url}?obtener=periodos&idPlantel=${plantel}`);
 
-    if(!respuesta) throw 'No se pudo obtener los niveles';
+    if(!respuesta.ok) throw 'No se pudo obtener los niveles';
 
     const periodos = await respuesta.json();
     //console.log(niveles);
@@ -45,7 +45,7 @@ const obtenerCarreras = async ( modo, nivel, periodo, plantel ) => {
 
     const respuesta = await fetch(`${url}?obtener=carreras&idModo=${modo}&idNivel=${nivel}&idPeriodo=${periodo}&idPlantel=${plantel}`);
 
-    if(!respuesta) throw 'No se pudo obtener los niveles';
+    if(!respuesta.ok) throw 'No se pudo obtener los niveles';
 
     const carreras = await respuesta.json();
     //console.log(niveles);
@@ -57,12 +57,35 @@ const obtenerHorarios = async ( carrera, nivel, periodo, plantel ) => {
 
     const respuesta = await fetch(`${url}?obtener=horarios&&idCarrera=${carrera}&idNivel=${nivel}&idPeriodo=${periodo}&idPlantel=${plantel}`);
 
-    if(!respuesta) throw 'No se pudo obtener los niveles';
+    if(!respuesta.ok) throw 'No se pudo obtener los niveles';
 
     const horarios = await respuesta.json();
     return horarios;
 
 };
+
+const guardarDatos = async ( datos ) => {
+
+    //console.log(datos);
+    //return datos;
+
+    
+    const respuesta = await fetch(`${url}?obtener=guardaDatos`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    })
+
+    if(!respuesta.ok) throw 'No se pudo guardar los datos';
+
+    const datosGuardados = await respuesta.json();
+    //console.log(datosGuardados);
+    return datosGuardados;
+    
+}
 
 
 
@@ -71,5 +94,6 @@ export {
     obtenerNiveles,
     obtenerPeriodos,
     obtenerCarreras,
-    obtenerHorarios
+    obtenerHorarios,
+    guardarDatos
 }
