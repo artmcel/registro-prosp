2cf57c10ba5363fb4e30049a55931c39c77477e9
WebMaster <umrec_web@unimex.edu.mx>
2022-05-14 09:35:36 -0500

    add contenido object

---
+++ src/js/contenido.js
@@ -0,0 +1,54 @@
+export const contenido = [
+    {
+        "izcalli" : {
+            "tituloPrincipal": "",
+            "ubicacion" : "",
+            "titulo1" : "",
+            "titulo2" : "",
+            "titulo3" : "",
+            "ofertaAcademica" : "",
+            "srcFotos" : ""
+
+        },
+        "satelite" : {
+            "tituloPrincipal": "",
+            "ubicacion" : "",
+            "titulo1" : "",
+            "titulo2" : "",
+            "titulo3" : "",
+            "ofertaAcademica" : "",
+            "srcFotos" : ""
+
+        },
+        "polanco": {
+            "tituloPrincipal": "",
+            "ubicacion" : "",
+            "titulo1" : "",
+            "titulo2" : "",
+            "titulo3" : "",
+            "ofertaAcademica" : "",
+            "srcFotos" : ""
+
+        },
+        "veracruz": {
+            "tituloPrincipal": "",
+            "ubicacion" : "",
+            "titulo1" : "",
+            "titulo2" : "",
+            "titulo3" : "",
+            "ofertaAcademica" : "",
+            "srcFotos" : ""
+
+        },
+        "veracruzSua": {
+            "tituloPrincipal": "",
+            "ubicacion" : "",
+            "titulo1" : "",
+            "titulo2" : "",
+            "titulo3" : "",
+            "ofertaAcademica" : "",
+            "srcFotos" : ""
+
+        }
+    }
+];
