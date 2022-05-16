c0d6416ff01a0c110c96ce08b63577b55db4961f
WebMaster <umrec_web@unimex.edu.mx>
2022-05-16 13:32:34 -0500

    add bootstrap budle and pooper

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@
 import './styles.css';
 //import bootsrap si lo necesitas
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@popperjs/core';
 //import './js/componentes.js';
  
  //cargadinamica ejemplo:
