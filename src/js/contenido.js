89dc57d3d3f16f6ddc7cf4d24f5d89b42941854f
WebMaster <umrec_web@unimex.edu.mx>
2022-05-16 17:39:18 -0500

    update content for each plantel

--- src/js/contenido.js
+++ src/js/contenido.js
@@ -39,44 +39,140 @@ const contenido = [
 
         },
         "satelite": {
-            "tituloPrincipal": "",
-            "ubicacion": "",
-            "titulo1": "",
-            "titulo2": "",
-            "titulo3": "",
-            "ofertaAcademica": "",
-            "srcFotos": ""
-
+            "plantel": "SATÉLITE",
+            "tituloPrincipal1": "RECONOCIMIENTO",
+            "tituloPrincipal2": "DE VALIDEZ",
+            "tituloPrincipal3": "ANTE LA SEP",
+            "ubicacion": "Circuito Poetas No. 37 (frente a Circuito Novelistas No. 41) Cd. Satélite C.P. 53100 Naucalpan de Juárez, Estado de México.",
+            "titulo1": "OBTÉN UNA BECA",
+            "titulo2": " 60% DE DESCUENTO",
+            "titulo3": "EN TURNO MATUTINO",
+            "ofertaAcademica": [
+                {
+                    "Administración": "Administración",
+                    "Administración de Empresas Turísticas": "Administración de Empresas Turísticas",
+                    "Comercio Internacional y Aduanas": "Comercio Internacional y Aduanas",
+                    "Comunicación": "Comunicación",
+                    "Contaduría Pública": "Contaduría Pública",
+                    "Derecho": "Derecho",
+                    "Diseño Gráfico": "Diseño Gráfico",
+                    "Idiomas": "Idiomas",
+                },
+                {
+                    "Mercadotecnia y Publicidad": "Mercadotecnia y Publicidad",
+                    "Psicología Social": "Psicología Social",
+                    "Relaciones Internacionales y Comercio Exterior": "Relaciones Internacionales y Comercio Exterior",
+                    "Sistemas Computacionales": "Sistemas Computacionales",
+                    "Turismo": "Turismo",
+                    "Informática Administrativa": "Informática Administrativa",
+                    "Pedagogía": "Pedagogía"
+                }
+            ],
+            "srcFotos": [
+                {
+                    "foto1": "",
+                    "foto2": "",
+                    "foto3": "",
+                }
+            ]
         },
         "polanco": {
-            "tituloPrincipal": "",
-            "ubicacion": "",
-            "titulo1": "",
-            "titulo2": "",
-            "titulo3": "",
-            "ofertaAcademica": "",
-            "srcFotos": ""
-
+            "plantel": "POLANCO",
+            "tituloPrincipal1": "TERMINA",
+            "tituloPrincipal2": "TU CARRERA",
+            "tituloPrincipal3": "EN 3 AÑOS 4 MESES",
+            "ubicacion": "Emilio Castelar No. 63, esq. Eugenio Sue, (Polanco o Auditorio). Col. Polanco-Chapultepec, C.P.11560, Ciudad de México",
+            "titulo1": "OBTÉN UNA BECA",
+            "titulo2": " 60% DE DESCUENTO",
+            "titulo3": "EN TURNO MATUTINO",
+            "ofertaAcademica": [
+                {
+                    "Administración": "Administración",
+                    "Administración de Empresas Turísticas": "Administración de Empresas Turísticas",
+                    "Comercio Internacional y Aduanas": "Comercio Internacional y Aduanas",
+                    "Comunicación": "Comunicación",
+                    "Contaduría Pública": "Contaduría Pública",
+                    "Derecho": "Derecho",
+                    "Diseño Gráfico": "Diseño Gráfico",
+                    "Idiomas": "Idiomas",
+                },
+                {
+                    "Mercadotecnia y Publicidad": "Mercadotecnia y Publicidad",
+                    "Psicología Social": "Psicología Social",
+                    "Relaciones Internacionales y Comercio Exterior": "Relaciones Internacionales y Comercio Exterior",
+                    "Sistemas Computacionales": "Sistemas Computacionales",
+                    "Turismo": "Turismo",
+                    "Informática Administrativa": "Informática Administrativa",
+                    "Pedagogía": "Pedagogía"
+                }
+            ],
+            "srcFotos": [
+                {
+                    "foto1": "",
+                    "foto2": "",
+                    "foto3": "",
+                }
+            ]
         },
         "veracruz": {
-            "tituloPrincipal": "",
-            "ubicacion": "",
-            "titulo1": "",
-            "titulo2": "",
-            "titulo3": "",
-            "ofertaAcademica": "",
-            "srcFotos": ""
-
+            "plantel": "VERACRUZ",
+            "tituloPrincipal1": "ESTUDIA Y TRABAJA",
+            "tituloPrincipal2": "CON NUESTRA VARIEDAD DE HORARIOS",
+            "ubicacion": "Av. 20 de noviembre esq. Juan Enríquez No. 1004 Veracruz, Vereracruz",
+            "titulo1": "OBTÉN UNA BECA",
+            "titulo2": " 60% DE DESCUENTO",
+            "titulo3": "EN TURNO MATUTINO",
+            "ofertaAcademica": [
+                {
+                    "Administración": "Administración",
+                    "Administración de Empresas Turísticas": "Administración de Empresas Turísticas",
+                    "Comercio Internacional y Aduanas": "Comercio Internacional y Aduanas",
+                    "Comunicación": "Comunicación",
+                    "Contaduría Pública": "Contaduría Pública",
+                    "Derecho": "Derecho",
+                    "Diseño Gráfico": "Diseño Gráfico",
+                    "Idiomas": "Idiomas",
+                },
+                {
+                    "Mercadotecnia y Publicidad": "Mercadotecnia y Publicidad",
+                    "Psicología Social": "Psicología Social",
+                    "Relaciones Internacionales y Comercio Exterior": "Relaciones Internacionales y Comercio Exterior",
+                    "Sistemas Computacionales": "Sistemas Computacionales",
+                    "Turismo": "Turismo",
+                    "Ciencias de la Educación": "Ciencias de la Educación",
+                    "Gastronomía Internacional": "Gastronomía Internacional"
+                }
+            ],
+            "srcFotos": [
+                {
+                    "foto1": "",
+                    "foto2": "",
+                    "foto3": "",
+                }
+            ]
         },
         "veracruzSua": {
-            "tituloPrincipal": "",
-            "ubicacion": "",
-            "titulo1": "",
-            "titulo2": "",
-            "titulo3": "",
-            "ofertaAcademica": "",
-            "srcFotos": ""
-
+            "plantel": "VERACRUZ",
+            "tituloPrincipal1": "ESTUDIA Y TRABAJA",
+            "tituloPrincipal2": "CON NUESTRA VARIEDAD DE HORARIOS",
+            "ubicacion": "Av. 20 de noviembre esq. Juan Enríquez No. 1004 Veracruz, Vereracruz",
+            "titulo1": "OBTÉN UNA BECA",
+            "titulo2": " 60% DE DESCUENTO",
+            "titulo3": "EN TURNO MATUTINO",
+            "ofertaAcademica": [
+                {
+                    "Administración": "Administración",
+                    "Contaduría Pública": "Contaduría Pública",
+                    "Derecho": "Derecho",
+                },
+            ],
+            "srcFotos": [
+                {
+                    "foto1": "",
+                    "foto2": "",
+                    "foto3": "",
+                }
+            ]
         }
     }
 ];
